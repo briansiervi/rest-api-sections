1ec962912a8cb866d4e0aec818a7ecbf8096dbc5
brian <brian.siervi@ntendencia.com.br>
2018-09-21 21:26:18 -0300

    iteração contracionada

--- section2/4_if_statements.py
+++ section2/4_if_statements.py
@@ -25,45 +25,17 @@
 #     print(a)
 
 def who_do_you_know():
-    known_people = []
     persons = input("Enter the names of people you know, separated by commas: ")
-    person_list = persons.split(",")
-
-    for person in person_list:
-        known_people.append(person.strip().lower())
-
+    known_people = [person.strip().lower() for person in persons.split(",")]
     return known_people
 
+def ask_user():
+    person = input("Enter the person name: ")    
 
-def who_do_you_know2():
-    anyone = "y"
-    known_people = []
-
-    while anyone == "y":
-        person = input("Enter the person you know: ")
-        anyone = input("Do you know anyone else? (y/n) ")
-
-        known_people.append(person.lower())
-
-    return known_people
-
-
-def ask_user(commas):
-    person = input("Enter the person name: ")
-    you_know = False
-
-    if commas == True:
-        if person.lower() in who_do_you_know():
-            you_know = True        
-    else:
-        if person.lower() in who_do_you_know2():
-            you_know = True
-    
-    if you_know:
+    if person.lower() in who_do_you_know():
         print("You know {}!".format(person))
     else:
         print("You don't know {}!".format(person))
 
 
-ask_user(True)
-# ask_user(False)
+ask_user()
