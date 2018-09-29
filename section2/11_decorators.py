20b126aa294e6b5f4cea02f02469c650bd87e041
Brian Mazini Siervi <brianmazini@hotmail.com>
2018-09-29 19:50:41 -0300

    decorator mais complexo

--- section2/11_decorators.py
+++ section2/11_decorators.py
@@ -13,3 +13,24 @@ def my_function():
     print("I'm the function")
 
 my_function()
+
+##
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_thet_runs_fun(*args, **kwargs):
+            print("In the decorator!")
+            if number == 56:
+                print("Not running the function")
+            else:
+                func(*args, **kwargs)
+            print("After the decorator!")
+        return function_thet_runs_fun
+    return my_decorator
+
+@decorator_with_arguments(56)
+def my_function_too():
+    print("Hello")
+
+my_function_too(57, 67)
