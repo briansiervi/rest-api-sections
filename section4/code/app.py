b4e46bf54b7c9e388166601c3d5a46f703863c67
brian <brian.siervi@ntendencia.com.br>
2018-10-11 17:39:26 -0300

    Tratamento de post duplicado

--- section4/code/app.py
+++ section4/code/app.py
@@ -6,14 +6,15 @@ api = Api(app)
 
 items = []
 
-class Item(Resource):    
+class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)        
+        return {'item': item}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': 'An item with name "{}" already exists'.format(name)}, 400
+            
         data = request.get_json() #get_json(force=True) / get_json(silent=True)
         item = {'name': name, 'price': data['price']}
         items.append(item)
