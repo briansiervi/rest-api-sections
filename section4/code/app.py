89279883ab7bde56e50623b2977763525a94e836
Brian Mazini Siervi <brianmazini@hotmail.com>
2018-10-12 19:18:00 -0300

    ambiente virtual do linux configurado

--- section4/code/app.py
+++ section4/code/app.py
@@ -15,13 +15,13 @@ items = []
 class Item(Resource):
     @jwt_required()
     def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)        
+        item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': 'An item with name "{}" already exists'.format(name)}, 400
-            
+
         data = request.get_json() #get_json(force=True) / get_json(silent=True)
         item = {'name': name, 'price': data['price']}
         items.append(item)
